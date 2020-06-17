2215da6680cb92564dc4fc8e55183b779e424082
Nicholas Sharp <naas8905@gmail.com>
2020-06-17 08:43:07 -0700

    Adding simple files

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Workflow from './workflow';
+import Rules from './rules';
+import Discussion from './discussion';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <Discussion />
+        <Rules />
+        <Workflow />
       </div>
     );
   }
